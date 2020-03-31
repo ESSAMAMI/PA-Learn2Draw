068039133e50c041d9149fec794e100b2a859914
Dreamsplutox <arnaudsimon091@gmail.com>
2020-03-31 22:07:29 +0200

    first commit home_page

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ import json
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def home():
+def home(token=None):
+    try:
+        current_year = utils.get_ccurent_date(format="ang", full=False)
+        return render_template("index.html", token=token, current_year=current_year)
+
+    except Exception as e:
+        current_date = utils.get_ccurent_date(format="fr")
+        return render_template("index.html", error=e)
+
+@app.route('/connection', methods=['GET'])
+def connection_page():
     try:
         current_year = utils.get_ccurent_date(format="ang", full=False)
         token = utils.generate_token()
