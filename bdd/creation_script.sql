46d49441a19c3ad191f2fe4d49394d75dd47f2f7
ESSAMAMI <hamza.essamami.sio@gmail.com>
2020-04-04 13:11:43 +0200

    error fixed in bdd :card_file_box:

--- bdd/creation_script.sql
+++ bdd/creation_script.sql
@@ -8,20 +8,20 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema learn2draw_db
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `learn2draw_db` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema learn2draw_db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `learn2draw_db` DEFAULT CHARACTER SET utf8 ;
+USE `learn2draw_db` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`USERS`
+-- Table `learn2draw_db`.`USERS`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`USERS` (
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`USERS` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(200) NOT NULL,
   `email` VARCHAR(200) NOT NULL,
@@ -32,20 +32,20 @@ CREATE TABLE IF NOT EXISTS `mydb`.`USERS` (
 ENGINE = InnoDB;
 
 
+-- ---------------------------------------------------
+-- Table `learn2draw_db`.`CATEGORY`
 -- -----------------------------------------------------
--- Table `mydb`.`CATEGORY`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CATEGORY` (
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`CATEGORY` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`DRAWING`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`DRAWING` (
+-- -- -----------------------------------------------------
+-- -- Table `learn2draw_db`.`DRAWING`
+-- -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`DRAWING` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `USERS_id` INT NOT NULL,
   `CATEGORY_id` INT NOT NULL,
@@ -53,52 +53,52 @@ CREATE TABLE IF NOT EXISTS `mydb`.`DRAWING` (
   `status` CHAR(1) NOT NULL DEFAULT 'U',
   `score` INT NULL,
   `time` INT NOT NULL,
-  PRIMARY KEY (`id`, `USERS_id`, `CATEGORY_id`),
-  INDEX `fk_DRAWING_USERS_idx` (`USERS_id` ASC) VISIBLE,
-  INDEX `fk_DRAWING_CATEGORY1_idx` (`CATEGORY_id` ASC) VISIBLE,
-  CONSTRAINT `fk_DRAWING_USERS`
-    FOREIGN KEY (`USERS_id`)
-    REFERENCES `mydb`.`USERS` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_DRAWING_CATEGORY1`
-    FOREIGN KEY (`CATEGORY_id`)
-    REFERENCES `mydb`.`CATEGORY` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`, `USERS_id`, `CATEGORY_id`))
 ENGINE = InnoDB;
 
+--  CONSTRAINT FK DRAWING USERS | CATEGORY
+ALTER TABLE `learn2draw_db`.`DRAWING` ADD
+CONSTRAINT `fk_DRAWING_USERS_idx`
+FOREIGN KEY (`USERS_id`)
+REFERENCES `learn2draw_db`.`USERS` (`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
 
--- -----------------------------------------------------
--- Table `mydb`.`NOTATION`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`NOTATION` (
+ALTER TABLE `learn2draw_db`.`DRAWING` ADD
+CONSTRAINT `fk_DRAWING_CATEGORY1_idx`
+FOREIGN KEY (`CATEGORY_id`)
+REFERENCES `learn2draw_db`.`CATEGORY` (`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- -- -----------------------------------------------------
+-- -- Table `learn2draw_db`.`NOTATION`
+-- -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`NOTATION` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `score` VARCHAR(200) NOT NULL,
   `USERS_id` INT NOT NULL,
   `DRAWING_id` INT NOT NULL,
   `DRAWING_USERS_id` INT NOT NULL,
   `DRAWING_CATEGORY_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `USERS_id`, `DRAWING_id`, `DRAWING_USERS_id`, `DRAWING_CATEGORY_id`),
-  INDEX `fk_NOTATION_USERS1_idx` (`USERS_id` ASC) VISIBLE,
-  INDEX `fk_NOTATION_DRAWING1_idx` (`DRAWING_id` ASC, `DRAWING_USERS_id` ASC, `DRAWING_CATEGORY_id` ASC) VISIBLE,
-  CONSTRAINT `fk_NOTATION_USERS1`
-    FOREIGN KEY (`USERS_id`)
-    REFERENCES `mydb`.`USERS` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_NOTATION_DRAWING1`
-    FOREIGN KEY (`DRAWING_id` , `DRAWING_USERS_id` , `DRAWING_CATEGORY_id`)
-    REFERENCES `mydb`.`DRAWING` (`id` , `USERS_id` , `CATEGORY_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`, `USERS_id`, `DRAWING_id`, `DRAWING_USERS_id`, `DRAWING_CATEGORY_id`))
 ENGINE = InnoDB;
 
+ALTER TABLE `learn2draw_db`.`NOTATION` ADD
+CONSTRAINT `fk_NOTATION_USERS1_idx`
+FOREIGN KEY (`USERS_id`)
+REFERENCES `learn2draw_db`.`USERS` (`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `learn2draw_db`.`NOTATION` ADD
+CONSTRAINT `fk_NOTATION_DRAWING1`
+FOREIGN KEY (`DRAWING_id` , `DRAWING_USERS_id` , `DRAWING_CATEGORY_id`)
+ REFERENCES `learn2draw_db`.`DRAWING` (`id` , `USERS_id` , `CATEGORY_id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
 
 -- -----------------------------------------------------
--- Table `mydb`.`MODELS`
+-- Table `learn2draw_db`.`MODELS`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`MODELS` (
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`MODELS` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NOT NULL,
   `epochs` INT NOT NULL,
@@ -109,24 +109,22 @@ CREATE TABLE IF NOT EXISTS `mydb`.`MODELS` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `mydb`.`RULES`
+-- Table `learn2draw_db`.`RULES`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`RULES` (
+CREATE TABLE IF NOT EXISTS `learn2draw_db`.`RULES` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_param` VARCHAR(200) NULL,
   `second_param` VARCHAR(200) NULL,
   `MODELS_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `MODELS_id`),
-  INDEX `fk_RULES_MODELS1_idx` (`MODELS_id` ASC) VISIBLE,
-  CONSTRAINT `fk_RULES_MODELS1`
-    FOREIGN KEY (`MODELS_id`)
-    REFERENCES `mydb`.`MODELS` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`, `MODELS_id`))
 ENGINE = InnoDB;
 
+ALTER TABLE `learn2draw_db`.`RULES` ADD
+CONSTRAINT `fk_RULES_MODELS1_idx`
+FOREIGN KEY (`MODELS_id`)
+REFERENCES `learn2draw_db`.`MODELS` (`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
